0b69a29f892f076f1e553ca665e0782c81e6880c
vivilis85 <vivipaez381@gmail.com>
2022-11-21 16:47:26 -0500

    se crea cinema

--- src/controllers/listadoPeliculas.js
+++ src/controllers/listadoPeliculas.js
@@ -2,53 +2,64 @@ let peliculas=[
     {
         nombre:"BLACK",
         duracion:120,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/black.png?alt=media&token=354fc0dd-b7a6-4b80-a69c-65c032c3b75a"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/black.png?alt=media&token=354fc0dd-b7a6-4b80-a69c-65c032c3b75a",
+        sinopsis:"Mavela, una adolescente de quince años, nunca conseguía encajar, hasta que entra en los Black Bronx, una conocida banda de jóvenes africanos. Allí se siente como en casa, es aceptada y, por primera vez, siente que está donde debe estar. Un día en comisaría conoce a Marwan, un magrebí de la banda rival y se enamoran. Juntos emprenderán una relación clandestina."
 
     },
     {
         nombre:"COCODRILO",
         duracion:90,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/cocodrilo.jpg?alt=media&token=28f78224-6034-40f8-9079-5adef8c66a79"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/cocodrilo.jpg?alt=media&token=28f78224-6034-40f8-9079-5adef8c66a79",
+        sinopsis:"Un equipo de televisión viaja al continente africano para hacer un reportaje sobre un temible depredador que ha matado a más de 300 personas. Allí, un criminal africano complicará su misión."
     },
     {
         nombre:"FEDORA",
         duracion:85,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/fedora.png?alt=media&token=70d257dd-61ed-4b4e-9166-12cbe5430dce"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/fedora.png?alt=media&token=70d257dd-61ed-4b4e-9166-12cbe5430dce",
+        sinopsis:"A base de flasbacks, recorremos la misteriosa vida de Fedora, una famosa actriz de Hollywood que muere atropellada."
     },
     {
         nombre:"WAKANDA",
         duracion:104,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/wakanda.jpg?alt=media&token=aae4abc8-c8d8-4d6a-a08a-a5695e84177e"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/wakanda.jpg?alt=media&token=aae4abc8-c8d8-4d6a-a08a-a5695e84177e",
+        sinopsis:"Una secuela que seguirá explorando el incomparable mundo de Wakanda y todos los ricos y variados personajes presentados en la película de 2018."
     },
     {
         nombre: "HABITACION",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/habitacion.png?alt=media&token=2a239102-fbf1-4279-8223-17ad9fe350c3"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/habitacion.png?alt=media&token=2a239102-fbf1-4279-8223-17ad9fe350c3",
+        sinopsis:"Desde que nació hace cinco años, Jack vive junto a su cariñosa y entregada madre en , el único mundo que el niño conoce, un pequeño cuarto en el que el captor de su madre los tiene retenidos."
     },
     {
         nombre: "SONRIE",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/sonrie.jpg?alt=media&token=a4fb2b48-fc63-491c-92df-6a9ad3ee6e61"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/sonrie.jpg?alt=media&token=a4fb2b48-fc63-491c-92df-6a9ad3ee6e61",
+        sinopsis:"Después de ser testigo de un extraño y traumático accidente que involucró a una paciente, la Dr. Rose Cotter (Sosie Bacon) empieza a experimentar sucesos aterradores que no puede explicarse. A medida que el terror comienza a apoderarse de su vida, Rose debe enfrentarse a su pasado para sobrevivir y escapar de su horrible nueva realidad."
     },
     {
         nombre: "LUZ DEL DIABLO",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/luz.png?alt=media&token=2da1f260-48b0-4c4d-8d7f-7a213ce5fdf5"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/luz.png?alt=media&token=2da1f260-48b0-4c4d-8d7f-7a213ce5fdf5",
+        sinopsis:"Según informes reales del Vaticano, los casos de posesión demoníaca han aumentado significativamente en los últimos años. En respuesta, la iglesia católica ha reabierto en secreto escuelas de exorcismo para capacitar a los sacerdotes en este rito sagrado. LA LUZ DEL DIABLO te sumerge en el mundo de una de estas escuelas; última línea de defensa de la humanidad contra los poderes del mal eterno. Jacqueline Byers (“Roadies”, “Salvation”) interpreta a la hermana Ann, quien cree devotamente que realizar exorcismos es su vocación, a pesar de que históricamente sólo los sacerdotes, y no las hermanas, pueden realizarlos. Cuando un profesor siente su don especial decide aceptarla en el aula, lo que le permite ser la primera monja en estudiar y dominar el ritual. Su propia alma estará en peligro ya que las fuerzas demoníacas con las que lucha revelan una conexión misteriosa con su traumático pasado."
     },
     {
         nombre: "REENCARNACION",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/reencarnacion.png?alt=media&token=bc27562b-80fc-4dfb-8a04-80bd4739fe2e"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/reencarnacion.png?alt=media&token=bc27562b-80fc-4dfb-8a04-80bd4739fe2e",
+        sinopsis:"Anna es una viuda que está a punto de empezar una nueva vida cuando aparece un niño que afirma ser la reencarnación de su difunto esposo."
     },
     {
         nombre: "REYES DEL MUNDO",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/reyesdelmundo.jpg?alt=media&token=8b2b648a-c4a6-4826-84e4-e4c56616225d"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/reyesdelmundo.jpg?alt=media&token=8b2b648a-c4a6-4826-84e4-e4c56616225d",
+        sinopsis:"Un día todos los hombres se quedaron dormidos… Y los cercos de la tierra, ardieron” Rá (19), Culebro (16), Sere (14), Winny (12) Nano (13) cinco chicos de la calle de Medellín. Cinco reyes sin reino. En sus manos tienen unas viejas escrituras de una tierra heredada por Rá. Tras recibir una carta de la oficina de restitución de tierras del gobierno, notificando la devolución de los terrenos que le fueron arrebatados a su abuela, Rá decidirá emprender el viaje con sus amigos con la promesa de por fin cumplir un sueño: tener un lugar en el mundo donde ser libres, donde estar a salvo, donde construir su propio reino."
     },
+ 
     {
         nombre: "LA TRAVIATA",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/traviata.png?alt=media&token=13822812-5dd4-40ab-8d0e-eccbc8008ebe"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemadavid-d68c1.appspot.com/o/traviata.png?alt=media&token=13822812-5dd4-40ab-8d0e-eccbc8008ebe",
+        sinopsis:"Radiotelevisione Italiana (RAI) Sinopsis: Violetta Valéry (Teresa Stratas) es una cortesana de los altos círculos parisinos amenazada por la tisis y el aturdimiento, que un buen día se enamora de un rico heredero provinciano, Alfredo Germont (Plácido Domingo), que la ama ardientemente y ansía librarla de sus males."
     }
 ]
 
@@ -68,21 +79,49 @@ peliculas.forEach(function(pelicula){
     poster.classList.add("img-fluid","w-100")
     poster.src = pelicula.poster
 
-    let nombre = document.createElement("h1")
-    nombre.classList.add("text-center","fw-bold")
+    let  duracion = document.createElement("h3")
+    duracion.classList.add("text-center")
+    duracion.textContent = "Duracion " + pelicula.duracion + "Min"
+
+    let nombre = document.createElement("h2")
+    nombre.classList.add("text-center")
     nombre.textContent = pelicula.nombre
 
-    let duracion = document.createElement("h5")
-    duracion.classList.add("text-start")
-    duracion.textContent= "Duracion: " + pelicula.duracion + "Min"
+    let sinopsis = document.createElement("p")
+    sinopsis.classList.add("d-none")
+    sinopsis.textContent = pelicula.sinopsis
+
 
    //padres e hijos (jerarquia de etiquetas)
 
     etiquetaFila.appendChild(columna)
+    tarjeta.appendChild(duracion)
+    tarjeta.appendChild(nombre)
     columna.appendChild(tarjeta)
     tarjeta.appendChild(poster)
-    tarjeta.appendChild(nombre)
-    tarjeta.appendChild(duracion)
-   
+    tarjeta.appendChild(sinopsis)
+    
+})
+
+
+//escuchando el evento de click en alguna pelicula de la cartelera
+let peliculaSeleccionada = {}
+etiquetaFila.addEventListener("click",function(evento){
+    if(evento.target.classList.contains("img-fluid")){
+       
+       //obteniendo los datos de la pelicula
+       peliculaSeleccionada.imagen = evento.target.parentElement.querySelector("img").src
+       peliculaSeleccionada.titulo = evento.target.parentElement.querySelector("h3").textContent
+       peliculaSeleccionada.duracion = evento.target.parentElement.querySelector("h2").textContent
+       peliculaSeleccionada .sinopsis = evento.target.parentElement.querySelector("p").textContent
+
+       //enviando nuestros datos de la pelicula seleccionada a la memoria
+       localStorage.setItem("pelicula",JSON.stringify(peliculaSeleccionada))
+
+       window.location.href = "./src/views/verinfopeliculas.html"
+
+
+    }
+
     
 })
